13ecc51a93a718f6bf6bb1a2133199fb1d2b8156
Lech Ogórkiewicz <feliscatus@interia.eu>
2023-02-10 11:45:13 +0100

    changed schedule

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -17,7 +17,6 @@ public class EmailScheduler {
     private final AdminConfig adminConfig;
 
     @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String message = "Currently in database you got " + size + " task";
